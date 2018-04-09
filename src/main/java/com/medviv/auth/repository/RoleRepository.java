6da1981c12e86892f8b6d6e01410b53683029868
Jay Paulynice <julesbond007@users.noreply.github.com>
2018-04-09 10:44:12 -0400

    clean up

--- src/main/java/com/medviv/auth/repository/RoleRepository.java
+++ src/main/java/com/medviv/auth/repository/RoleRepository.java
@@ -1,9 +1,7 @@
 package com.medviv.auth.repository;
 
 import java.io.Serializable;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.medviv.auth.repository.entity.RoleEntity;
 
 public interface RoleRepository extends JpaRepository<RoleEntity, Serializable> {
