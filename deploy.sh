91299ef330e6d34fa5185911635d1c5a0ce42142
Jay Paulynice <julesbond007@users.noreply.github.com>
2018-04-11 18:36:13 -0400

    clean up

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cp -r provider/build/libs/provider-1.0.war /usr/local/tomcat/webapps/provider.war
-cp -r resource/build/libs/resource-1.0.war /usr/local/tomcat/webapps/resource.war
+cp -r provider/build/libs/provider-1.0.war /usr/local/Cellar/tomcat/8.5.21/libexec/webapps/provider.war
+cp -r resource/build/libs/resource-1.0.war /usr/local/Cellar/tomcat/8.5.21/libexec/webapps/resource.war
